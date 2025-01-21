8b8bde34b9def54ecaa4dc0f262990ffd87dbd21
nelcandido <nelcandido@gmail.com>
2025-01-21 20:52:36 -0300

    feat: Dia-4 - ECS: Autoscaling de Aplicações

--- service.tf
+++ service.tf
@@ -16,6 +16,12 @@ resource "aws_ecs_service" "main" {
     rollback = true
   }
 
+
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
   network_configuration {
     security_groups = [
       aws_security_group.main.id
